5a5ff4e4e64bc5eaedaeaafee4511884bb28a897
Fiona Rowan <fmr2112@columbia.edu>
2018-07-18 11:19:05 -0400

    twilio functionality

--- src/Pin.js
+++ src/Pin.js
@@ -8,17 +8,22 @@ const ITEM_TYPES = {
 }
 
 class Pin extends Component {
+
+  getLocationLink() {
+    return "http://maps.google.com/maps?q=" + this.props.position[0] + "," + this.props.position[1];
+  }
+
   render() {
     let popupText = '';
     switch(this.props.type) {
       case ITEM_TYPES.COMPOST:
-        popupText = 'compost here!!';
+        popupText = 'Food scraps drop off point.';
         break;
       case ITEM_TYPES.RECYCLING:
-        popupText = 'recycle here!!';
+        popupText = 'Public Recycling.';
         break;
       case ITEM_TYPES.ELECTRONICS:
-        popupText = 'electronics here!!';
+        popupText = 'Electronics drop off point.';
         break;
       default:
         popupText = '';
@@ -26,7 +31,16 @@ class Pin extends Component {
     return(
       <Marker position={this.props.position}>
         <Popup>
-          <span>{popupText}</span>
+          <div>
+          <div>
+            <span>{popupText}</span>
+            <label>{' '}Enter a number to receive this location.{' '}</label>
+            <input id="number" type="text" name="text"/>
+          </div>
+          <div>
+            <input type="submit" value="Send" onClick={() => this.props.textClient(document.getElementById('number').value, this.getLocationLink())}/>
+          </div>
+          </div>
         </Popup>
       </Marker>
     );
